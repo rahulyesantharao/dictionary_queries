ed8c075221922fbeb21806997139df28a4b5fb25
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2019-11-17 14:04:41 -0500

    reformat

--- part1.cpp
+++ part1.cpp
@@ -1,6 +1,3 @@
-#include "cJSON/cJSON.h"
-#include "simdjson/singleheader/simdjson.cpp"
-#include "simdjson/singleheader/simdjson.h"
 #include <fstream>
 #include <iostream>
 #include <list>
@@ -8,6 +5,9 @@
 #include <unordered_map>
 #include <utility>
 #include <vector>
+#include "cJSON/cJSON.h"
+#include "simdjson/singleheader/simdjson.cpp"
+#include "simdjson/singleheader/simdjson.h"
 using namespace simdjson;
 
 //--- hardcoded cases ---
@@ -113,7 +113,7 @@ cJSON *BuildCJSONHelper(ParsedJson::Iterator &pjh,
         pjh.next();
         // get value
         cur_stack.push_back(cur_key);
-        cJSON *val = BuildCJSONHelper(pjh, cur_stack); // recurse
+        cJSON *val = BuildCJSONHelper(pjh, cur_stack);  // recurse
 
         // determine whether this key should be part of the final
         // representation
@@ -122,11 +122,9 @@ cJSON *BuildCJSONHelper(ParsedJson::Iterator &pjh,
           if (cur_stack.size() <= keys_to_keep[i].size()) {
             bool good = true;
             for (int j = 0; good && j < cur_stack.size(); j++) {
-              if (cur_stack[j] != keys_to_keep[i][j])
-                good = false;
+              if (cur_stack[j] != keys_to_keep[i][j]) good = false;
             }
-            if (good)
-              should_insert = true;
+            if (good) should_insert = true;
           }
         }
         cur_stack.pop_back();
@@ -142,37 +140,37 @@ cJSON *BuildCJSONHelper(ParsedJson::Iterator &pjh,
     ret = cJSON_CreateArray();
     if (pjh.down()) {
       do {
-        cJSON *val = BuildCJSONHelper(pjh, cur_stack); // recurse
+        cJSON *val = BuildCJSONHelper(pjh, cur_stack);  // recurse
         cJSON_AddItemToArray(ret, val);
       } while (pjh.next());
       pjh.up();
     }
-  } else { // single type
+  } else {  // single type
     switch (pjh.get_type()) {
-    case '"': // string
-      ret = cJSON_CreateString(pjh.get_string());
-      break;
-    case 'l':
-      ret = cJSON_CreateNumber((double)pjh.get_integer());
-      break;
-    case 'u':
-      ret = cJSON_CreateNumber((double)pjh.get_unsigned_integer());
-      break;
-    case 'd':
-      ret = cJSON_CreateNumber(pjh.get_double());
-      break;
-    case 'n': // we have a null
-      ret = cJSON_CreateNull();
-      break;
-    case 't': // we have a true
-      ret = cJSON_CreateTrue();
-      break;
-    case 'f': // we have a false
-      ret = cJSON_CreateFalse();
-      break;
-    default:
-      std::cerr << "Error in cJSON Parsing!" << std::endl;
-      ret = nullptr;
+      case '"':  // string
+        ret = cJSON_CreateString(pjh.get_string());
+        break;
+      case 'l':
+        ret = cJSON_CreateNumber((double)pjh.get_integer());
+        break;
+      case 'u':
+        ret = cJSON_CreateNumber((double)pjh.get_unsigned_integer());
+        break;
+      case 'd':
+        ret = cJSON_CreateNumber(pjh.get_double());
+        break;
+      case 'n':  // we have a null
+        ret = cJSON_CreateNull();
+        break;
+      case 't':  // we have a true
+        ret = cJSON_CreateTrue();
+        break;
+      case 'f':  // we have a false
+        ret = cJSON_CreateFalse();
+        break;
+      default:
+        std::cerr << "Error in cJSON Parsing!" << std::endl;
+        ret = nullptr;
     }
   }
   return ret;
@@ -206,7 +204,7 @@ void *BuildHashmapHelper(ParsedJson::Iterator &pjh,
         // get type
         char type = (char)pjh.get_type();
         // get value
-        void *val = BuildHashmapHelper(pjh, cur_stack); // recurse
+        void *val = BuildHashmapHelper(pjh, cur_stack);  // recurse
 
         // determine whether this key should be part of the final
         // representation
@@ -215,11 +213,9 @@ void *BuildHashmapHelper(ParsedJson::Iterator &pjh,
           if (cur_stack.size() <= keys_to_keep[i].size()) {
             bool good = true;
             for (int j = 0; good && j < cur_stack.size(); j++) {
-              if (cur_stack[j] != keys_to_keep[i][j])
-                good = false;
+              if (cur_stack[j] != keys_to_keep[i][j]) good = false;
             }
-            if (good)
-              should_insert = true;
+            if (good) should_insert = true;
           }
         }
         cur_stack.pop_back();
@@ -237,45 +233,45 @@ void *BuildHashmapHelper(ParsedJson::Iterator &pjh,
     if (pjh.down()) {
       do {
         char type = (char)pjh.get_type();
-        void *val = BuildHashmapHelper(pjh, cur_stack); // recurse
+        void *val = BuildHashmapHelper(pjh, cur_stack);  // recurse
         arr->push_back(std::make_pair(type, val));
       } while (pjh.next());
       pjh.up();
     }
-  } else { // single type
+  } else {  // single type
     switch (pjh.get_type()) {
-    case '"': { // string
-      char *val = new char[pjh.get_string_length()];
-      memcpy(val, pjh.get_string(), pjh.get_string_length());
-      ret = (void *)val;
-    } break;
-    case 'l': {
-      int64_t *val = new int64_t;
-      *val = pjh.get_integer();
-      ret = (void *)val;
-    } break;
-    case 'u': {
-      uint64_t *val = new uint64_t;
-      *val = pjh.get_unsigned_integer();
-      ret = (void *)val;
-    } break;
-    case 'd': {
-      double *val = new double;
-      *val = pjh.get_double();
-      ret = (void *)val;
-    } break;
-    case 'n': // we have a null
-      ret = nullptr;
-      break;
-    case 't':        // we have a true
-      ret = nullptr; // already stored in key string
-      break;
-    case 'f':        // we have a false
-      ret = nullptr; // already stored in key string
-      break;
-    default:
-      std::cerr << "Error in cJSON Parsing!" << std::endl;
-      ret = nullptr;
+      case '"': {  // string
+        char *val = new char[pjh.get_string_length()];
+        memcpy(val, pjh.get_string(), pjh.get_string_length());
+        ret = (void *)val;
+      } break;
+      case 'l': {
+        int64_t *val = new int64_t;
+        *val = pjh.get_integer();
+        ret = (void *)val;
+      } break;
+      case 'u': {
+        uint64_t *val = new uint64_t;
+        *val = pjh.get_unsigned_integer();
+        ret = (void *)val;
+      } break;
+      case 'd': {
+        double *val = new double;
+        *val = pjh.get_double();
+        ret = (void *)val;
+      } break;
+      case 'n':  // we have a null
+        ret = nullptr;
+        break;
+      case 't':         // we have a true
+        ret = nullptr;  // already stored in key string
+        break;
+      case 'f':         // we have a false
+        ret = nullptr;  // already stored in key string
+        break;
+      default:
+        std::cerr << "Error in cJSON Parsing!" << std::endl;
+        ret = nullptr;
     }
   }
   return ret;
@@ -288,52 +284,50 @@ void PrintHashmapValue(Any val) {
   char type = val.first;
   void *ptr = val.second;
   switch (type) {
-  case '{': {
-    Hashmap *hashmap = (Hashmap *)ptr;
-    std::cout << "{";
-    int count = 0;
-    for (Hashmap::iterator it = hashmap->begin(); it != hashmap->end();
-         ++it, ++count) {
-      std::string key = it->first;
+    case '{': {
+      Hashmap *hashmap = (Hashmap *)ptr;
+      std::cout << "{";
+      int count = 0;
+      for (Hashmap::iterator it = hashmap->begin(); it != hashmap->end();
+           ++it, ++count) {
+        std::string key = it->first;
 
-      if (count > 0)
-        std::cout << ",";
-      std::cout << "\"" << key << "\":";
-      PrintHashmapValue(it->second);
-    }
-    std::cout << "}";
-  } break;
-  case '[': {
-    std::list<Any> *arr = (std::list<Any> *)ptr;
-    int count = 0;
-    for (std::list<Any>::iterator it = arr->begin(); it != arr->end();
-         ++it, ++count) {
-      if (count > 0)
-        std::cout << ",";
-      PrintHashmapValue(*it);
-    }
-  } break;
-  case '"': // string
-    std::cout << "\"" << (const char *)ptr << "\"";
-    break;
-  case 'l':
-    std::cout << *(int64_t *)ptr;
-    break;
-  case 'u':
-    std::cout << *(uint64_t *)ptr;
-    break;
-  case 'd':
-    std::cout << *(double *)ptr;
-    break;
-  case 'n': // we have a null
-    std::cout << "null";
-    break;
-  case 't': // we have a true
-    std::cout << "true";
-    break;
-  case 'f': // we have a false
-    std::cout << "false";
-    break;
+        if (count > 0) std::cout << ",";
+        std::cout << "\"" << key << "\":";
+        PrintHashmapValue(it->second);
+      }
+      std::cout << "}";
+    } break;
+    case '[': {
+      std::list<Any> *arr = (std::list<Any> *)ptr;
+      int count = 0;
+      for (std::list<Any>::iterator it = arr->begin(); it != arr->end();
+           ++it, ++count) {
+        if (count > 0) std::cout << ",";
+        PrintHashmapValue(*it);
+      }
+    } break;
+    case '"':  // string
+      std::cout << "\"" << (const char *)ptr << "\"";
+      break;
+    case 'l':
+      std::cout << *(int64_t *)ptr;
+      break;
+    case 'u':
+      std::cout << *(uint64_t *)ptr;
+      break;
+    case 'd':
+      std::cout << *(double *)ptr;
+      break;
+    case 'n':  // we have a null
+      std::cout << "null";
+      break;
+    case 't':  // we have a true
+      std::cout << "true";
+      break;
+    case 'f':  // we have a false
+      std::cout << "false";
+      break;
   }
 }
 
@@ -368,7 +362,7 @@ void *BuildFlattenedStructHelper(ParsedJson::Iterator &pjh,
         char type = (char)pjh.get_type();
         // get value
         void *val =
-            BuildFlattenedStructHelper(pjh, cur_stack, build); // recurse
+            BuildFlattenedStructHelper(pjh, cur_stack, build);  // recurse
         // set the appropriate field of the struct if needed
         SetFlattenedStructField(cur_stack, std::make_pair(type, val), build);
         cur_stack.pop_back();
@@ -384,47 +378,47 @@ void *BuildFlattenedStructHelper(ParsedJson::Iterator &pjh,
         cur_stack.push_back(std::to_string(index));
         char type = (char)pjh.get_type();
         void *val =
-            BuildFlattenedStructHelper(pjh, cur_stack, build); // recurse
+            BuildFlattenedStructHelper(pjh, cur_stack, build);  // recurse
         SetFlattenedStructField(cur_stack, std::make_pair(type, val), build);
         cur_stack.pop_back();
         index++;
       } while (pjh.next());
       pjh.up();
     }
-  } else { // single type
+  } else {  // single type
     switch (pjh.get_type()) {
-    case '"': { // string
-      char *val = new char[pjh.get_string_length()];
-      memcpy(val, pjh.get_string(), pjh.get_string_length());
-      ret = (void *)val;
-    } break;
-    case 'l': {
-      int64_t *val = new int64_t;
-      *val = pjh.get_integer();
-      ret = (void *)val;
-    } break;
-    case 'u': {
-      uint64_t *val = new uint64_t;
-      *val = pjh.get_unsigned_integer();
-      ret = (void *)val;
-    } break;
-    case 'd': {
-      double *val = new double;
-      *val = pjh.get_double();
-      ret = (void *)val;
-    } break;
-    case 'n': // we have a null
-      ret = nullptr;
-      break;
-    case 't':        // we have a true
-      ret = nullptr; // already stored in type char
-      break;
-    case 'f':        // we have a false
-      ret = nullptr; // already stored in type char
-      break;
-    default:
-      std::cerr << "Error in Struct Parsing!" << std::endl;
-      ret = nullptr;
+      case '"': {  // string
+        char *val = new char[pjh.get_string_length()];
+        memcpy(val, pjh.get_string(), pjh.get_string_length());
+        ret = (void *)val;
+      } break;
+      case 'l': {
+        int64_t *val = new int64_t;
+        *val = pjh.get_integer();
+        ret = (void *)val;
+      } break;
+      case 'u': {
+        uint64_t *val = new uint64_t;
+        *val = pjh.get_unsigned_integer();
+        ret = (void *)val;
+      } break;
+      case 'd': {
+        double *val = new double;
+        *val = pjh.get_double();
+        ret = (void *)val;
+      } break;
+      case 'n':  // we have a null
+        ret = nullptr;
+        break;
+      case 't':         // we have a true
+        ret = nullptr;  // already stored in type char
+        break;
+      case 'f':         // we have a false
+        ret = nullptr;  // already stored in type char
+        break;
+      default:
+        std::cerr << "Error in Struct Parsing!" << std::endl;
+        ret = nullptr;
     }
   }
   return ret;
@@ -440,8 +434,7 @@ void SetFlattenedStructField(std::vector<std::string> &cur_stack, Any val,
     if (cur_stack.size() == keys_to_keep[i].size()) {
       bool good = true;
       for (int j = 0; good && j < cur_stack.size(); j++) {
-        if (cur_stack[j] != keys_to_keep[i][j])
-          good = false;
+        if (cur_stack[j] != keys_to_keep[i][j]) good = false;
       }
       if (good) {
         should_insert = true;
@@ -453,17 +446,17 @@ void SetFlattenedStructField(std::vector<std::string> &cur_stack, Any val,
   // add the pair to the object if it is desired
   if (should_insert) {
     switch (i) {
-    case 0:
-      build->_1_created_at = (char *)val.second;
-      break;
-    case 1:
-      build->_1_text = (char *)val.second;
-    case 2:
-      build->_2_user_id = *(int64_t *)val.second;
-    case 3:
-      build->_2_user_screen_name = (char *)val.second;
-    case 4:
-      build->_2_user_followers_count = *(int64_t *)val.second;
+      case 0:
+        build->_1_created_at = (char *)val.second;
+        break;
+      case 1:
+        build->_1_text = (char *)val.second;
+      case 2:
+        build->_2_user_id = *(int64_t *)val.second;
+      case 3:
+        build->_2_user_screen_name = (char *)val.second;
+      case 4:
+        build->_2_user_followers_count = *(int64_t *)val.second;
     }
   }
 }
