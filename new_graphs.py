ec3ba70197c630e3cd3ccda52bafaa4213be6f7b
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2020-05-18 21:56:37 -0500

    bunch of images and data for runs

--- new_graphs.py
+++ new_graphs.py
@@ -3,9 +3,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
-case = 3
-data_set = "T"
-hue_color = "#c41200" if data_set == "Y" else "#00ACEE"
+case = 4
+data_set = "Y"
+hue_color = "#25ccf7" if data_set == "T" else "#EA7773"
+
+# hue_color = "#c41200" if data_set == "Y" else "#00ACEE"
 
 if case == 1:
     # build time
@@ -19,17 +21,18 @@ elif case == 2:
     image_name = "rAccess{}".format(data_set)
     col = "r_access"
     col_to_graph = "r_access"
-    data_label = "Access Time (s)"
+    data_label = "Total Access Time (s)"
 elif case == 3:
     # compile time
     filename = "final_data/dataa{}.csv".format(data_set)
     image_name = "aAccess{}".format(data_set)
     col = "c_access"
     col_to_graph = "c_access"
-    data_label = "Access Time (s)"
+    data_label = "Total Access Time (s)"
 else:
     # compile time
-    filename = "datam.csv"
+    filename = "final_data/datam{}.csv".format(data_set)
+    image_name = "mem{}".format(data_set)
     col = "memory"
     col_to_graph = "memory"
     data_label = "Memory Usage (GB)"
@@ -53,6 +56,7 @@ if col == "build":
 df.groupby(["dataset", "type"])[col].describe()
 
 sns.set_style("whitegrid")
+# sns.set(font_scale=2)
 pal = [sns.color_palette("Blues_d", 9)[2], sns.color_palette("Blues_d", 9)[7]]
 sns.set_palette(pal)
 plt.figure(figsize=(10, 5))
@@ -61,22 +65,27 @@ ax = sns.barplot(
     y=col_to_graph,
     hue="dataset",
     data=df,
-    order=["Nested Hashmap", "Tree", "Indexed String", "Flattened Struct"],
+    order=["Tree", "Nested Hashmap", "Indexed String", "Flattened Struct"],
     palette=[hue_color],
 )
 plt.xlabel("")
-plt.ylabel(data_label, fontsize=16)
-plt.xticks(fontsize=14)
-plt.yticks(fontsize=14)
+plt.ylabel("{}\n".format(data_label), fontsize=28)
+plt.xticks(fontsize=30)
+ax.set_xticklabels(
+    ["Nested\nTree", "Nested\nHashmap", "Indexed\nString", "Flattened\nStruct"]
+)
+# ax.set_xticklabels(ax.get_xticklabels(), rotation=40, ha="right")
+plt.yticks(fontsize=30)
 sns.despine()
 
 handles, labels = ax.get_legend_handles_labels()
-ax.legend(
-    handles=handles,
-    labels=[l.capitalize() for l in labels],
-    fontsize=16,
-    loc="upper right",
-)
+# ax.legend(
+# handles=handles,
+# labels=[l.capitalize() for l in labels],
+# fontsize=16,
+# loc="upper right",
+# )
+ax.legend_.remove()
 # plt.ylim(0, 10)
 plt.tight_layout()
-plt.savefig("{}.pdf".format(image_name), dpi=120, transparent=True)
+plt.savefig("final_graphs/{}.pdf".format(image_name), dpi=120, transparent=True)
